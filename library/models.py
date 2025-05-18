355e26e8993d2495704f0654e89ff30cfc112154
N1ler4 <bahtiyorov.nozim@gmail.com>
2025-05-18 22:49:42 +0500

    homework

---
+++ library/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_year = models.IntegerField()
+    genre = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
